f62bccd24d75738f46b11a353ffaad8a6a1f7e4a
Eugene <comradeeugene@gmail.com>
2022-01-22 00:07:07 +0300

    first

--- Lesson_3.3/Program.cs
+++ Lesson_3.3/Program.cs
@@ -6,7 +6,35 @@ namespace Lesson_3
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите целое положительное число");
+            int CheckValue = int.Parse(Console.ReadLine());
+            int i = 2;
+            if (CheckValue == 1)
+            {
+                Console.WriteLine($"{CheckValue} не простое число");
+            }
+            else if (CheckValue > 1)
+            {
+                while (i < CheckValue)
+                {
+                    if (CheckValue % i == 0)
+                    {
+                        Console.WriteLine($"{CheckValue} не простое число");
+                        goto M1;
+                    }
+                    else
+                    {    
+                        ++i;
+                    }
+                }
+                Console.WriteLine($"{CheckValue} простое число");
+            }
+            else
+            {
+                Console.WriteLine("Не корректное значение");
+            }
+        M1:
+            Console.ReadKey();
         }
     }
 }
